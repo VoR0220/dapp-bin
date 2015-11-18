e1d948a90db78de9b71d394e86bf9f49a1101087
VoR0220 <catalanor0220@gmail.com>
2015-11-18 11:41:15 -0600

    Update math.sol

--- library/math.sol
+++ library/math.sol
@@ -8,4 +8,30 @@ library Math {
             x = mulmod(x, x, m);
         }
     }
+    
+    /// @dev Computes the square root of x
+    function sqrt(uint x) returns (uint) {
+        uint y = x;
+        while( true ) {
+            uint z = (y + (x/y))/2;
+            uint w = (z + (x/z))/2;
+            if( w == y) {
+                if( w < y ) return w;
+                else return y;
+            }
+            y = w;
+        }
+    }
+
+    /// @dev calculates euclidean distance of point A and B
+    function eucledianDistance(uint x_a, uint y_a,  uint x_b, uint y_b) returns (uint) {
+        return sqrt((x_a - y_b) ** 2 + (y_a, - x_b) ** 2);
+    }
+    
+    
+    function interpolate(uint x_a, uint y_a, uint x_b, uint y_b, uint delta) returns (uint x, uint y) {
+        x = x_a * delta + x_b * delta;
+        y = y_a * delta + y_b *delta;
+        return x, y;
+    }
 }
